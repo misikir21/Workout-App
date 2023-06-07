c0904a75c19bb66f1e472f4785093b7fb553548c
misikira <misikirteka@gmail.com>
2023-06-07 10:54:50 +0300

    get the x and y cordinates

--- starter/script.js
+++ starter/script.js
@@ -10,3 +10,16 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+/////////////////////////////////////////////////////////////////////
+//add location
+
+navigator.geolocation.getCurrentPosition(function(position){
+    const {latitude}=position.coords;
+    const {longitude}=position.coords;
+    console.log(latitude,longitude)
+    console.log(position)
+},
+function(){
+        alert('you not get your location')
+    }
+)
