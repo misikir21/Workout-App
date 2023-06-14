e75be96d40d7c95cfe563e114861f3fc0187d35e
misikira <misikirteka@gmail.com>
2023-06-14 12:15:19 +0300

    add the app class with 4 methods

--- starter/script.js
+++ starter/script.js
@@ -14,38 +14,57 @@ const inputElevation = document.querySelector('.form__input--elevation');
 /////////////////////////////////////////////////////////////////////
 //add location
 let map,mapEvent;
-navigator.geolocation.getCurrentPosition(function(position){
-    const {latitude}=position.coords;
-    const {longitude}=position.coords;
-    const cords=[latitude,longitude]
-    console.log(latitude,longitude)
-    console.log(`https://www.google.pt/maps/@${latitude},${longitude}`)
-
-
-    map = L.map('map').setView(cords, 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-//add location to map on click
-map.on('click',function(mapE){
-mapEvent=mapE;
-form.classList.remove('hidden')
-inputDistance.focus()
-
-})
-
-},
-
-function(){
-        alert('soory we can not get your location')
+class App{
+    #map;
+    #mapEvent;
+    constructor(){
+        this._getPosition();
     }
-);
+
+    _getPosition(){
+        if(navigator.geolocation)
+        navigator.geolocation.getCurrentPosition (this._loadMap.bind(this),function(){
+                alert('soory we can not get your location')
+            })
+        }
 
 
+
+    _loadMap(position){
+            const {latitude}=position.coords;
+            const {longitude}=position.coords;
+            const cords=[latitude,longitude]
+            console.log(latitude,longitude)
+            console.log(`https://www.google.pt/maps/@${latitude},${longitude}`)
+        
+        
+            this.#map = L.map('map').setView(cords, 13);
+        
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.#map);
+        //add location to map on click
+        this.#map.on('click',function(mapE){
+        this.#mapEvent=mapE;
+        form.classList.remove('hidden')
+        inputDistance.focus()
+        
+        })
+        
+        }
+        
+    }
+    // _toggleElevationField(){}
+    // _newWorkouT(){}
+
+
+
+const app=new App()
 //display marker 
 form.addEventListener('submit',function(e){
     e.preventDefault()
+    //clear the input feilds
+    inputDistance.value=inputDuration.value=inputElevation.value=inputCadence.value='';
     const{lat,lng}=mapEvent.latlng;
 L.marker([lat,lng])
 .addTo(map)
@@ -62,4 +81,9 @@ L.popup({
 .setPopupContent('workout')
 .openPopup();
     
+})
+
+inputType.addEventListener('change',function(){
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
 })
